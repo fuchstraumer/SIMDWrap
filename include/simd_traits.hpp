cd232f3acd1df1ee4dccd17a1109f51af9dd1143
Keegan <jonesk74@uw.edu>
2018-09-11 15:23:12 -0700

    Remove storage_proxy, add other files we plan to use soon

--- include/simd_traits.hpp
+++ include/simd_traits.hpp
@@ -6,61 +6,65 @@
 #include <type_traits>
 #include <immintrin.h>
 
-struct x64_platform_tag{};
-struct x86_platform_tag{};
-struct arm_platform_tag{};
+namespace sw {
 
-// will be set by CMake script eventually. will be used
-// to enable and use NEON types on ARM.
-using platform_type = x64_platform_tag;
-// will also be set by CMake, eventually
-static constexpr bool USE_AVX_INTRINSICS = true;
+    struct x64_platform_tag {};
+    struct x86_platform_tag {};
+    struct arm_platform_tag {};
 
-namespace detail {
+    // will be set by CMake script eventually. will be used
+    // to enable and use NEON types on ARM.
+    using platform_type = x64_platform_tag;
+    // will also be set by CMake, eventually
+    static constexpr bool USE_AVX_INTRINSICS = true;
 
-    template<typename T, size_t LEN>
-    struct vector_type_proxy {
-        constexpr auto operator()() const noexcept {
-            if constexpr (!std::is_same_v<platform_type, arm_platform_tag>) {
-                if constexpr (std::is_same_v<T, float> && LEN <= 4) {
-                    return __m128();
+    namespace detail {
+
+        template<typename T, size_t LEN>
+        struct vector_type_proxy {
+            constexpr static auto get_type() noexcept {
+                if constexpr (!std::is_same_v<platform_type, arm_platform_tag>) {
+                    if constexpr (std::is_same_v<T, float> && LEN <= 4) {
+                        return __m128();
+                    }
+                    else if constexpr (std::is_same_v<T, double> && LEN <= 2) {
+                        return __m128d();
+                    }
+                    else if constexpr (std::is_same_v<platform_type, x64_platform_tag> && USE_AVX_INTRINSICS) {
+                        if constexpr (std::is_same_v<T, double> && LEN <= 4 && LEN > 2) {
+                            return __m256d();
+                        }
+                        else if constexpr (std::is_same_v<T, float> && LEN <= 8 && LEN > 4) {
+                            return __m256();
+                        }
+                        else if constexpr (std::is_integral_v<T> && USE_AVX_INTRINSICS) {
+                            return __m256i();
+                        }
+                    }
                 }
-                else if constexpr (std::is_same_v<T, double> && LEN <= 2) {
-                    return __m128d();
-                }
-                else if constexpr (std::is_same_v<platform_type, x64_platform_tag> && USE_AVX_INTRINSICS) {
-                    if constexpr (std::is_same_v<T, double> && LEN <= 4 && LEN > 2) {
-                        return __m256d();
-                    }
-                    else if constexpr (std::is_same_v<T, float> && LEN <= 8 && LEN > 4) {
-                        return __m256();
-                    }
-                    else if constexpr (std::is_integral_v<T> && USE_AVX_INTRINSICS) {
-                        return __m256i();
-                    }
+                else {
+                    // need to add ARM case here.
+                    return T();
                 }
             }
-            else {
-                // need to add ARM case here.
-                return T();
-            }
-        }
-    };
+            using type = decltype(get_type());
+        };
+
+    }
 
     template<typename T, size_t LEN>
-    constexpr bool is_simd_compatible =!std::is_same_v<T, std::result_of_t<vector_type_proxy<T, LEN>()>>;
+    constexpr bool is_simd_compatible = !std::is_same_v<T, detail::vector_type_proxy<T, LEN>::type >> ;
 
     template<typename T, size_t LEN>
-    constexpr size_t vectorized_alignment = alignof(std::result_of_t<vector_type_proxy<T, LEN>()>);
+    constexpr size_t vectorized_alignment = alignof(detail::vector_type_proxy<T, LEN>::type);
 
     template<typename T, size_t LEN>
     struct simd_traits {
-        using vector_type = std::result_of_t<vector_type_proxy<T, LEN>()>;
+        using vector_type = detail::vector_type_proxy<T, LEN>::type;
         constexpr static size_t num_entries = sizeof(vector_type) / sizeof(T);
         constexpr static size_t remainder_entries = num_entries % LEN;
-        constexpr static size_t alignment = vectorized_alignment<T,LEN>;
+        constexpr static size_t alignment = vectorized_alignment<T, LEN>;
     };
 
 }
-
 #endif //!SIMD_WRAP_SIMD_TRAITS_HPP
---
+++ include/vector.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef SIMD_WRAP_VECTOR_HPP
+#define SIMD_WRAP_VECTOR_HPP
+#include "simd_traits.hpp"
+
+namespace sw {
+
+    template<typename T, size_t LEN>
+    struct vector {
+        using underlying_vector_type = typename simd_traits<T, LEN>::vector_type;
+        static_assert(is_simd_compatible<T, LEN>, "Given combination of data type T and vector length LEN is not SIMD-compatible!");
+        constexpr vector() noexcept;
+        template<typename...Args>
+        constexpr vector(Args...args) noexcept;
+    private:
+        underlying_vector_type data;
+    };
+
+}
+
+#include "vector.inl"
+
+#endif // !SIMD_WRAP_VECTOR_HPP
